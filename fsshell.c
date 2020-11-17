5ecd24cadd109e252d80880dedf60efc04330671
Feras Alayoub <falayoub@mail.sfsu.edu>
2020-11-16 17:00:37 -0800

    Lauren request

--- fsshell.c
+++ fsshell.c
@@ -152,8 +152,8 @@ int displayFiles(fdDir *dirp, int flall, int fllong)
 		// {
 		// 	if (fllong)
 		// 	{
-				fs_stat(di->d_name, &statbuf);
-				printf("%s    %9ld   %s\n", fs_isDir(di->d_name) ? "D" : "-", statbuf.st_size, di->d_name);
+				//fs_stat(di->d_name, &statbuf);
+				//printf("%s    %9ld   %s\n", fs_isDir(di->d_name) ? "D" : "-", statbuf.st_size, di->d_name);
 		// 	}
 		// 	else
 		// 	{
